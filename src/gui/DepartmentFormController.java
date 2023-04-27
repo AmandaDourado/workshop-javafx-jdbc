62c0c3f5a031e89b207e748e6b3afb086c441e63
Amanda Dourado <amanda_lopes1998@hotmail.com>
2023-04-26 21:35:38 -0300

    Update department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -18,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import model.entities.Department;
 import model.exceptions.ValidationException;
@@ -45,6 +46,9 @@ public class DepartmentFormController implements Initializable{
 	
 	@FXML
 	private Button btCancel;
+	
+	@FXML
+	TableColumn<Department, Department> tableColumnEDIT;
 	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
